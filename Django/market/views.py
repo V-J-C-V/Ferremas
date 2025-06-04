f3daac8da1d70d51cdbea90c94852673e8ee5642
V_J_C_V <vicente.javier.carrasco.vargas@gmail.com>
2025-06-03 21:31:57 -0400

    Cambio Alexander

--- Django/market/views.py
+++ Django/market/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+import requests
 
 def inicio(request):
     return render(request, "index.html")
+def obtener_catalogo():
+    url = "http://127.0.0.1:8088/api/Trabajador/"
+    try:
+        response = requests.json(url)
+        data = response.json()
+        return data
+    except  Exception as e:
+        return None
--- Gestion/demo/src/main/java/com/example/demo/controller/DetalleController.java
+++ Gestion/demo/src/main/java/com/example/demo/controller/DetalleController.java
@@ -3,9 +3,12 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.example.demo.entity.Detalle;
 import com.example.demo.repository.DetalleRepository;
@@ -18,6 +21,15 @@ public class DetalleController {
     public List<Detalle> ListarDetalle(){
         
         return detalleRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Detalle BuscarDetallePorId(@PathVariable Integer id){
+        return detalleRepository.findById(id)
+        .orElseThrow(() -> new ResponseStatusException(
+            HttpStatus.NOT_FOUND,
+            "No se encuentra"
+        ));
     }
 
 }
