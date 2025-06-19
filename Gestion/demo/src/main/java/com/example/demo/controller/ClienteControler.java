f23cf07f3b53510ba29b9930758d7dc50d41bbe3
V_J_C_V <vicente.javier.carrasco.vargas@gmail.com>
2025-06-19 16:44:21 -0400

    Push put

--- Gestion/demo/src/main/java/com/example/demo/controller/ClienteControler.java
+++ Gestion/demo/src/main/java/com/example/demo/controller/ClienteControler.java
@@ -4,15 +4,22 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import com.example.demo.entity.Cargo;
 import com.example.demo.entity.Cliente;
 import com.example.demo.repository.ClienteRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 @RestController
@@ -33,6 +40,32 @@ public Cliente BuscarClientePorId(@PathVariable String id){
         HttpStatus.NOT_FOUND,
         "No se enconro el cliente"
     ));
+}
+
+   @PostMapping
+    public ResponseEntity<Cliente> AgregarCliente(@RequestBody Cliente cliente){
+        Cliente  clienteGuardado = clienteRepository.save(cliente);
+        return ResponseEntity.ok(clienteGuardado);
+    }
+
+@PutMapping("/{id}")
+public ResponseEntity<Cliente> ActualizarCliente(@PathVariable String id, @RequestBody Cliente clienteactualizar){
+
+    Cliente clientexistente = clienteRepository.findById(id)
+    .orElseThrow(() -> new ResponseStatusException(
+
+    HttpStatus.NOT_FOUND,
+    "No se encontro"
+    ));
+
+
+    clientexistente.setNombre(clienteactualizar.getNombre());
+    clientexistente.setComuna(clienteactualizar.getComuna());
+    clientexistente.setRut(clienteactualizar.getRut());
+    clientexistente.setTelefono(clienteactualizar.getTelefono());
+    clientexistente.setUsuario(clienteactualizar.getUsuario());
+    Cliente clienteGuardado = clienteRepository.save(clientexistente);
+    return ResponseEntity.ok(clienteGuardado);
 }
 
 
